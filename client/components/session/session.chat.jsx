1e2929c83c0e2ad0c580f0ad0d12d9b73220c9e7
Felix Su <felixs8696@berkeley.edu>
2016-10-14 17:48:33 -0700

    basic chat box formatting

--- client/components/session/session.chat.jsx
+++ client/components/session/session.chat.jsx
@@ -3,8 +3,11 @@ import React from 'react';
 export default class SessionChat extends React.Component {
   render() {
     return (
-      <div>
-
+      <div className="chat-container">
+        <div className="reply-box">
+          <div className="attachment-button"></div>
+          <div className="reply-input"></div>
+        </div>
       </div>
     )
   }
