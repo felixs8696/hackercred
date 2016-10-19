908f8aefe909d98a0f2a8e35ee2f3ae723be010e
Felix Su <felixs8696@berkeley.edu>
2016-10-19 06:17:57 -0700

    added ability to switch video thumbnails on the sidebar

--- client/components/session/session.video.jsx
+++ client/components/session/session.video.jsx
@@ -1,5 +1,31 @@
 import React from 'react';
 
+Colors = {};
+Colors.names = {
+  red: '#DB4437',
+  lightblue: '#2294E3',
+  green: '#0F9D58',
+  yellow: '#F4C20D'
+}
+
+Colors.choose = function(i) {
+  var keys = Object.keys(this.names)
+  var key = keys[i % keys.length];
+  return this.names[key];
+};
+
+var replaceChildNode = function(node, type, className, style, text) {
+  node.removeChild(node.childNodes[0]);
+  var div = document.createElement(type);
+  div.className = className;
+  if (style) Object.assign(div.style,style);
+  if (text) {
+    var text = document.createTextNode(text);
+    div.appendChild(text);
+  }
+  node.appendChild(div);
+}
+
 export default class SessionVideo extends React.Component {
   componentDidMount() {
     var errorElement = document.querySelector('#error');
@@ -69,21 +95,30 @@ export default class SessionVideo extends React.Component {
         }
       }
       this._videoThumbnails = this._videoThumbnails.bind(this);
+      this._changeVideo = this._changeVideo.bind(this);
   }
 
   _videoThumbnails() {
-    return Object.keys(this.state.users).map((key) => {
-      console.log(key);
-      console.log(this.state.currentVideo);
+    return Object.keys(this.state.users).map((key, index) => {
+      this.state.users[key].color = Colors.choose(index);
       if (key == this.state.currentVideo) {
-        return (<div key={key} className="video-thumbnail" style={{ background: '#DB4437' }}>
-            <div className="video-user-picture" style={{ background: 'url(' + this.state.users[key].image + ')' }}></div>
+        return (<div key={key} className="video-thumbnail" id={key} value={key} style={{ background: this.state.users[key].color }} onClick={this._changeVideo}>
+            <div className="video-user-initial">{this.state.users[key].firstname[0]}</div>
           </div>);
       }
-      return (<div key={key} className="video-thumbnail">
-          <div className="video-user-picture" style={{ background: 'url(' + this.state.users[key].image + ')' }}></div>
+      return (<div key={key} className="video-thumbnail" id={key} value={key} onClick={this._changeVideo}>
+          <div className="video-user-picture" style={{ background: 'url(' + this.state.users[key].image + ')', border: '3px solid ' + this.state.users[key].color }}></div>
         </div>);
     });
+  }
+
+  _changeVideo(event) {
+    var previous = document.getElementById(this.state.currentVideo);
+    replaceChildNode(previous, "div", "video-user-picture", { background: 'url(' + this.state.users[this.state.currentVideo].image + ')', border: '3px solid ' + this.state.users[this.state.currentVideo].color });
+    var id = event.currentTarget.getAttribute("value");
+    replaceChildNode(event.currentTarget, "div", "video-user-initial", null, this.state.users[id].firstname[0]);
+    event.currentTarget.style.background = this.state.users[id].color;
+    this.setState({currentVideo: id});
   }
 
   render() {
