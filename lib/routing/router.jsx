402c355f4f28c193c9d64549f99e35cd1d84c06d
Felix Su <felixs8696@berkeley.edu>
2016-11-03 20:03:38 -0700

    added fast render

--- lib/routing/router.jsx
+++ lib/routing/router.jsx
@@ -1,21 +1,23 @@
-import React from 'react';
-import {mount} from 'react-mounter';
-import {Dashboard} from '/client/layouts/dashboard.jsx';
-import {Session} from '/client/layouts/session.jsx';
-import {Login} from '/client/layouts/login.jsx';
+if (Meteor.isClient) {
+  import React from 'react';
+  import {mount} from 'react-mounter';
+  import {Dashboard} from '/client/layouts/dashboard.jsx';
+  import {Session} from '/client/layouts/session.jsx';
+  import {Login} from '/client/layouts/login.jsx';
 
-import DashNavbar from '/client/components/dashboard/dash.navbar.jsx';
-import DashContent from '/client/components/dashboard/dash.content.jsx';
+  import DashNavbar from '/client/components/dashboard/dash.navbar.jsx';
+  import DashContent from '/client/components/dashboard/dash.content.jsx';
 
-import SessionNavbar from '/client/components/session/session.navbar.jsx';
-import SessionEditor from '/client/components/session/session.editor.jsx';
-import SessionVideo from '/client/components/session/session.video.jsx';
-import SessionChat from '/client/components/session/session.chat.jsx';
+  import SessionNavbar from '/client/components/session/session.navbar.jsx';
+  import SessionEditor from '/client/components/session/session.editor.jsx';
+  import SessionVideo from '/client/components/session/session.video.jsx';
+  import SessionChat from '/client/components/session/session.chat.jsx';
 
-import SessionChatContainer from '/client/containers/session.chat.js';
-import SessionEditorContainer from '/client/containers/session.editor.js';
-import SessionVideoContainer from '/client/containers/session.video.js';
-import LoginContainer from '/client/containers/login.js';
+  import SessionChatContainer from '/client/containers/session.chat.js';
+  import SessionEditorContainer from '/client/containers/session.editor.js';
+  import SessionVideoContainer from '/client/containers/session.video.js';
+  import LoginContainer from '/client/containers/login.js';
+}
 
 FlowRouter.route("/:sessionId?/login", {
   name: "login",
